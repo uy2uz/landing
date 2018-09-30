80b574231115718ae31572669fcaa7dbe433252e
AlexKuriakov <akuryakov@gmail.com>
2018-09-30 23:59:50 +0300

    v.0.0.9a

--- resources/views/admin/categories/add.blade.php
+++ resources/views/admin/categories/add.blade.php
@@ -6,7 +6,7 @@
         <form method="post">
             {!!csrf_field()!!}
             <p>Введите название категории:<br><input type="text" name="title" class="form-control" required></p>
-            <p>Описание каегории:<br><textarea type="text" name="description" class="form-control"></textarea></p>
+            <p>Описание категории:<br><textarea name="description" class="form-control"></textarea></p>
             <button type="submit" class="btn btn-success" style="float:right">Создать</button>
         </form>    
     </main>
--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -6,7 +6,7 @@
         <form method="post">
             {!!csrf_field()!!}
             <p>Введите название категории:<br><input type="text" name="title" value="{{$category->title}}" class="form-control" required></p>
-            <p>Описание каегории:<br><textarea type="text" name="description" value="{{$category->description}}" class="form-control" required></textarea></p>
+            <p>Описание категории:<br><textarea name="description" class="form-control">{!!$category->description!!}</textarea></p>
             <button type="submit" class="btn btn-success" style="float:right">Сохранить</button>
         </form>    
     </main>
